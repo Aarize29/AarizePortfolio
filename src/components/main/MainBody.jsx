26adbb7dd2b206c5f81e85c006370675cb7baa12
Aarize Siddique <97665948+Aarize29@users.noreply.github.com>
2024-01-23 23:15:20 +0530

    Update MainBody.jsx

--- src/components/main/MainBody.jsx
+++ src/components/main/MainBody.jsx
@@ -153,18 +153,18 @@ const MainBody = () => {
     <div  className=' mt-2 w-screen text-white flex p-5 flex-col'>
     <div className=' rounded-2xl  p-2 transform -scale-x-100 bg-repeat-space '>
           <div className='text-xl text-gray-300    mt-20 text-gray-300 text-center transform -scale-x-100'>
-        <div className='flex flex-col ml-5 mr-5  items-center' ><span className='text-fuchsia-500 mb-5 '>Intern At AvidSynth </span>
+        <div className='flex flex-col ml-5 mr-5  items-center' ><span className='text-fuchsia-500 mb-5 '>Intern At Audio AI, LLC </span>
         <span className='text-fuchsia-500'>June 2023 - Present</span>
         </div>
 
-        <p className='m-5'>As a full-stack intern at AvidSynth, I played a pivotal role in crafting the company's website, excelling in both frontend and backend development to deliver a seamless and dynamic user experience.
+        <p className='m-5'>As a full-stack intern at  Audio AI, LLC, I played a pivotal role in crafting the company's website, excelling in both frontend and backend development to deliver a seamless and dynamic user experience.
 
-        As a full-stack intern at AvidSynth, I skillfully utilized Node.js, React-Flow, Tailwind CSS, Firebase, and React.js .
+        As a full-stack intern at  Audio AI, LLC, I skillfully utilized Node.js, React-Flow, Tailwind CSS, Firebase, React.js and AWS services .
         </p>
         </div>
         <div className='text-xl text-gray-300    mt-20 text-gray-300 text-center transform -scale-x-100'>
         <div className='flex ml-5 mr-5 flex-col items-center' ><span className='text-fuchsia-500 mb-5'>Open Source Contributor At GSSOC2023 </span>
-        <span className='text-fuchsia-500'>May 2023 - Present</span>
+        <span className='text-fuchsia-500'>May 2023 - August 2023</span>
         </div>
 
         <p className='m-5'>As an open-source contributor at GirlScript Summer of Code, I actively contributed to various projects, collaborating with a diverse team to create meaningful solutions and empower the community through code.
